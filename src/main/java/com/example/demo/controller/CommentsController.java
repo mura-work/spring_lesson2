1c461ac3111073ad0df462ec18757e2845b1dcdd
murata <murata.naoya.infratop@gmail.com>
2021-05-13 20:51:55 +0900

    コメントの投稿機能作成

--- src/main/java/com/example/demo/controller/CommentsController.java
+++ src/main/java/com/example/demo/controller/CommentsController.java
@@ -11,18 +11,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.example.demo.entity.Comments;
 import com.example.demo.form.CommentForm;
 import com.example.demo.repository.CommentsRepository;
+import com.example.demo.repository.ItemRepository;
 
 @Controller
 public class CommentsController {
 	@Autowired
 	CommentsRepository repository;
 	
+	@Autowired
+	ItemRepository itemRepository;
+	
 	@PostMapping("/items/{id}/comments")
 	public String create(@Valid @ModelAttribute CommentForm form, @PathVariable int id) {
 		Comments comment = new Comments();
 		comment.setText(form.getText());
-		comment.setItem(form.getItem());
+		comment.setItem(itemRepository.getOne(id));
 		repository.save(comment);
-		return "redirect:/items/" + comment.getId();
+		return "redirect:/items/" + id;
 	}
 }
