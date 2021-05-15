87b1bd20d75faf3aeb0e1468697a6d78b084a03f
murata <murata.naoya.infratop@gmail.com>
2021-05-15 09:38:35 +0900

    退会機能作成

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import javax.validation.Valid;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,10 +1,18 @@
 package com.example.demo.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.example.demo.entity.User;
+import com.example.demo.form.UserForm;
 import com.example.demo.repository.UserRepository;
 
 @Controller
@@ -21,5 +29,38 @@ public class UserController {
 	public String index(Model model) {
 		model.addAttribute("users", repository.findAll());
 		return "/users/index";
+	}
+	
+	@GetMapping("/users/{id}")
+	public String show(@PathVariable int id, Model model) {
+		model.addAttribute("user", repository.getOne(id));
+		return "users/show";
+	}
+	
+	@GetMapping("/users/{id}/edit")
+	public String edit(@PathVariable int id, Model model) {
+		model.addAttribute("user", repository.getOne(id));
+		return "users/edit";
+	}
+	
+	@PatchMapping("/users/{id}")
+	public String update(@Valid @ModelAttribute UserForm form, BindingResult result, 
+			@PathVariable int id) {
+		if (result.hasErrors()) {
+			return "users/edit";
+		}
+		User user = repository.getOne(id);
+		user.setName(form.getName());
+		user.setPassword(form.getPassword());
+		repository.save(user);
+		return "redirect:/users/" + id;
+	}
+	
+	@PatchMapping("/users/{id}/reject")
+	public String rejectUser(@PathVariable int id) {
+		User user = repository.getOne(id);
+		user.setDeleted(true);
+		repository.save(user);
+		return "redirect:/";
 	}
 }
