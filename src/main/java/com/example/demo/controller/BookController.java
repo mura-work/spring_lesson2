df6aea22d69210dd4b9ec71b8c2af3655cd5d9e4
murata <murata.naoya.infratop@gmail.com>
2021-05-15 22:41:13 +0900

    削除まで

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -7,13 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.entity.Book;
+import com.example.demo.entity.User;
 import com.example.demo.form.BookForm;
 import com.example.demo.repository.BookRepository;
 import com.example.demo.repository.UserRepository;
@@ -42,7 +45,8 @@ public class BookController {
 		Book book = new Book();
 		book.setTitle(form.getTitle());
 		book.setText(form.getText());
-		book.setUser(userRepository.getOne((Integer)session.getAttribute("id")));
+		User user = userRepository.getOne((Integer)session.getAttribute("id"));
+		book.setUser(user);
 		repository.save(book);
 	return "redirect:/books";
 	}
@@ -60,8 +64,32 @@ public class BookController {
 	}
 	
 	@GetMapping("/books/{id}/edit")
-	public String edit(@ModelAttribute BookForm form) {
+	public String edit(@PathVariable int id, Model model) {
+		model.addAttribute("bookForm", repository.getOne(id));
 		return "books/edit";
+	}
+	
+	@PatchMapping("/books/{id}")
+	public String update(@Valid @ModelAttribute BookForm form, BindingResult result,
+			@PathVariable int id, RedirectAttributes flash) {
+		if (result.hasErrors()) {
+			flash.addFlashAttribute("org.springframework.validation.BindingRes ult.bookForm", result);
+			flash.addFlashAttribute("bookForm", form);
+			return "/books/edit";
+		}
+		Book book = repository.getOne(id);
+		book.setTitle(form.getTitle());
+		book.setText(form.getText());
+		book.setUser(userRepository.getOne(id));
+		repository.save(book);
+		return "redirect:/books/" + id;
+	}
+	
+	@DeleteMapping("/books/{id}")
+	public String update(@PathVariable int id, RedirectAttributes flash) {
+		repository.deleteById(id);
+		flash.addFlashAttribute("bookDelete", "ID：" + id + "を削除しました");
+		return "redirect:/books";
 	}
 }
 
