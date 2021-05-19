5eed303a7b3a24823c8b78514afd18343accd0ae
murata <murata.naoya.infratop@gmail.com>
2021-05-20 05:40:32 +0900

    genreコントローラ修正

--- src/main/java/com/example/demo/controller/GenreController.java
+++ src/main/java/com/example/demo/controller/GenreController.java
@@ -1,13 +1,11 @@
 package com.example.demo.controller;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -52,8 +50,13 @@ public class GenreController {
 		if (result.hasErrors()) {
 			return "genres/edit";
 		}
-		Optional<Genre> genre = repository.findById(id);
+		repository.save(repository.getById(id));
 		return "redirect:/genres";
-		
+	}
+	
+	@DeleteMapping("/genres/{id}")
+	public String delete(@PathVariable int id) {
+		repository.deleteById(id);
+		return "redirect:/genres";
 	}
 }
