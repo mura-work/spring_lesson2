e400c204664ceca652951388a8ee639051510cf1
murata <murata.naoya.infratop@gmail.com>
2021-05-22 17:38:06 +0900

    emp/newの作成

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,8 +1,21 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.form.EmployeeForm;
+import com.example.demo.repository.EmployeeRepository;
 
 @Controller
 public class EmployeeController {
-
+	@Autowired
+	EmployeeRepository repository;
+	
+	@GetMapping("/employee/new")
+	public String empNew(EmployeeForm form, Model model) {
+		model.addAttribute("employeeForm", form);
+		return "emp/new";
+	}
 }
