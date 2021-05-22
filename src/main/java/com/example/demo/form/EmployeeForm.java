b006096ae611e40167d772e9db7472c6bc8f906e
murata <murata.naoya.infratop@gmail.com>
2021-05-22 09:12:06 +0900

    エンティティ準備

---
+++ src/main/java/com/example/demo/form/EmployeeForm.java
@@ -0,0 +1,14 @@
+package com.example.demo.form;
+
+import java.util.Date;
+
+public class EmployeeForm {
+	private Integer empId;
+	private String empPass;
+	private String empName;
+	private Integer gender;
+	private String address;
+	private Date birthday;
+	private Integer authority;
+	private Integer deptId;
+}
