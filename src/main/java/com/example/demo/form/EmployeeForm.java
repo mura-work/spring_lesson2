385dca6ca462fe0e5ba3418352fe848752e4bd10
murata <murata.naoya.infratop@gmail.com>
2021-05-22 17:16:59 +0900

    formクラスの準備

--- src/main/java/com/example/demo/form/EmployeeForm.java
+++ src/main/java/com/example/demo/form/EmployeeForm.java
@@ -2,13 +2,88 @@ package com.example.demo.form;
 
 import java.util.Date;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class EmployeeForm {
+	
+	@Max(999)
 	private Integer empId;
+	
+	@NotBlank
+	@Size(max = 30)
 	private String empPass;
+	
+	@NotBlank
+	@Size(max = 16)
 	private String empName;
+	
+	@Max(2)
 	private Integer gender;
+	
+	@NotBlank
 	private String address;
+	
+	@NotBlank
 	private Date birthday;
+	
+	@NotBlank
+	@Max(2)
 	private Integer authority;
+	
 	private Integer deptId;
+	
+	public Integer getEmpId() {
+		return empId;
+	}
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+	public String getEmpPass() {
+		return empPass;
+	}
+	public void setEmpPass(String empPass) {
+		this.empPass = empPass;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public Integer getGender() {
+		return gender;
+	}
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public Integer getAuthority() {
+		return authority;
+	}
+	public void setAuthority(Integer authority) {
+		this.authority = authority;
+	}
+	public Integer getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(Integer deptId) {
+		this.deptId = deptId;
+	}
+	public EmployeeForm() {
+		this.gender = 1;
+		this.authority = 1;
+	}
 }
